2d9d0c3ce3200292fc941f62f8fd6edfd415a916
Aleksandre1996 <91327942+Aleksandre1996@users.noreply.github.com>
2021-10-06 17:51:43 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+//array
+
+let cars = ['audi','bmw', 'lexus', 'volkswagen',  'porsche', 'audi']; 
+            //0      1      2           3           4           5
+
+//for
+
+for(let i=0; i<6; i++){
+    console.log(cars[i]);
+}
+
+
+var person = {
+    firstname: 'john',
+    lastname: 'doe',
+    age: 50,
+    eyecolor: 'blue',
+  };
+console.log(person.eyecolor);
+
+let numbers= [15, 67, 100, 5, 7, [80, 500] ]; 
+            //0   1    2    3 4      5
+
+  console.log(numbers[5]);         
